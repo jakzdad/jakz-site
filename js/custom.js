908e1381adfa5e79ba100c82cc1beaf4f02756e8
BuildTools <unconfigured@null.spigotmc.org>
2016-12-11 12:47:48 -0500

    some bug fixes

--- js/custom.js
+++ js/custom.js
@@ -1,101 +1,117 @@
+// ==========================================================
+// Global Variables
+// ==========================================================
+
+// Class for each base
+var single = $('.first');
+var double = $('.second');
+var triple = $('.third');
+var homerun = $('.home');
+//Scoring Increase Variable
+var points = $('.point input').val();
+//Bases Array
+var bases = [
+  '.base .first',
+  '.base .second',
+  '.base .third',
+  '.base .home',
+]
+
+var currentBase = $('.score .hit');
+var nextBase = $('.score .hit + .base');
+var prevBase = currentBase.prev();
+
+// ==========================================================
+// Click "Hit" button to open hit options
+// ==========================================================
 
 
-$(document).ready(function(){
+$('.selection-hit').click(function(){
+  $('.hit-buttons').addClass('hit-buttons-show');
+});
+// ==========================================================
+// Click to hide any options menu
+// ==========================================================
 
-  // Click "Hit" button to open hit options
-  $('.selection-hit').click(function(){
-    $('.hit-buttons').addClass('hit-buttons-show');
+$('.hide').click(function(){
+ $('.hit-buttons').removeClass('hit-buttons-show');
+});
+
+// ===========================================================================
+// Click "reset" button to hide hit options, will also remove men from bases
+// ===========================================================================
+
+$('.reset').click(function(){
+  $('.hit-buttons').removeClass('hit-buttons-show');
+});
+// ==========================================================
+// if "reset" button is pressed take all men off base
+// ==========================================================
+
+for (var i = 0; i < bases.length; i++) {
+  $('.reset').click(function(){
+    $('.score .base').removeClass('hit');
   });
+}
 
-  // Click to hide any options menu
-  $('.hide').click(function(){
-   $('.hit-buttons').removeClass('hit-buttons-show');
- });
+// ==========================================================
+// Choosing the Type of at-bat
+// ==========================================================
 
+$('.hit-buttons button').click(function(e){
+  //Target of the hit selections buttons
+  var target = $(event.target);
 
-  $('.hit-buttons button').click(function(e){
-    //Target of the hit selections buttons
-    var target = $(event.target);
+  //Scoring Increase Variable
+  var points = $('.point input').val();
 
-    // Class for each base
-    var single = $('.first');
-    var double = $('.second');
-    var triple = $('.third');
-    var homerun = $('.home');
+  // if "single" button is pressed
+  if (target.hasClass('single') || target.hasClass('walk') || target.hasClass('hbp')) {
+    single.addClass('hit');
+    $('.hit-buttons').removeClass('hit-buttons-show');
+  }
 
-    //Bases Array
-    var bases = [
-      '.base .first',
-      '.base .second',
-      '.base .third',
-      '.base .home',
-    ]
+  // if "double" button is pressed
+  if (target.hasClass('double')) {
+    double.addClass('hit');
+    $('.hit-buttons').removeClass('hit-buttons-show');
+  }
+  // if "triple" button is pressed
+  if (target.hasClass('triple')) {
+    triple.addClass('hit');
+    $('.hit-buttons').removeClass('hit-buttons-show');
+  }
 
-    //Hit buttons array
-    var hitType = [
-      'hit-buttons button .single',
-      'hit-buttons button .double'
-    ]
+  // if "homerun" button is pressed, also adds 1 to the score
+  if (target.hasClass('homerun')) {
+    homerun.addClass('hit');
+    $('.hit-buttons').removeClass('hit-buttons-show');
+    points++;
+    $('.point input').val(points);
+  }
 
-    //Scoring Increase Variable
-    var points = $('.point input').val();
+});
 
-    // if "single" button is pressed
-    if (target.hasClass('single') || target.hasClass('walk') || target.hasClass('hbp')) {
-      single.addClass('hit');
-      $('.hit-buttons').removeClass('hit-buttons-show');
-    }
+// ==========================================================
+// Advance button
+// ==========================================================
 
-    // if "double" button is pressed
-    if (target.hasClass('double')) {
-      double.addClass('hit');
-      $('.hit-buttons').removeClass('hit-buttons-show');
-    }
-    // if "triple" button is pressed
-    if (target.hasClass('triple')) {
-      triple.addClass('hit');
-      $('.hit-buttons').removeClass('hit-buttons-show');
-    }
-    // if "homerun" button is pressed, also adds 1 to the score
-    if (target.hasClass('homerun')) {
-      homerun.addClass('hit');
-      $('.hit-buttons').removeClass('hit-buttons-show');
-      points++;
-      $('.point input').val(points);
-    }
+$('.selection-advance').click(function(e){
+  var currentBase = $('.score .hit');
+  var nextBase = $('.score .hit + .base');
+  var prevBase = currentBase.prev();
 
-    // if "reset" button is pressed take all men off base
-    for (var i = 0; i < bases.length; i++) {
-      $('.reset').click(function(){
-        $('.score .base').removeClass('hit');
-      });
-    }
+  nextBase.addClass('hit');
 
-    // =====================
-    // Advance button
-    // =====================
+  if (nextBase.is($('.home'))) {
+    $('.selection-advance').addClass('scored');
+  }
 
-    $('.selection-advance').click(function(){
-      var currentBase = $('.score .hit');
-      var nextBase = currentBase.next();
-      var single = $('.first');
-      var double = $('.second');
-      var triple = $('.third');
-      var homerun = $('.home');
-
-      nextBase.addClass('hit');
-      currentBase.removeClass('hit');
-
-      if (!nextBase.hasClass('hit')) {
-        alert("You don't have any runners to advance.");
-      }
-
-      if (currentBase.is(triple)) {
-        points++;
-        $('.point input').val(points);
-      }
-    });
-
-  });
+  if ($(event.target).hasClass('scored')) {
+    console.log('tits');
+    points = points + 1;
+    $('.point input').val(points);
+    $('.selection-advance').removeClass('scored');
+  }
 
 });
