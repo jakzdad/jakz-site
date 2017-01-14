1b51eb0bda66e821ef9be17b9573da077c4d128e
BuildTools <unconfigured@null.spigotmc.org>
2017-01-14 13:08:40 -0500

    components

---
+++ js/app.js
@@ -0,0 +1,69 @@
+var lucentApp = angular.module('lucentApp', ['ngRoute', 'ngResource']);
+
+// Routes
+// ================
+lucentApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+
+  $locationProvider.hashPrefix('');
+
+  $routeProvider.when('/', {
+    templateUrl: 'pages/splash.html',
+    controller: 'splashController'
+  })
+
+  .when('/home', {
+    templateUrl: 'pages/home.html',
+    controller: 'homeController'
+  })
+
+  .when('/about', {
+    templateUrl: 'pages/about.html',
+    controller: 'aboutController'
+  })
+
+  .when('/contact', {
+    templateUrl: 'pages/contact.html',
+    controller: 'contactController'
+  })
+
+  .when('/products', {
+    templateUrl: 'pages/products.html',
+    controller: 'productsController'
+  })
+
+  .otherwise({
+      redirectTo : '/'
+  });
+
+}]);
+
+lucentApp.controller('navController', ['$scope', function($scope){
+
+  //nav show and hide
+  $scope.navOpen = false;
+  $scope.drawerTigger = function() {
+    console.log('test');
+    $scope.navOpen = !$scope.navOpen;
+  };
+
+}]);
+
+lucentApp.controller('splashController', ['$scope', '$location', function($scope, $location){
+  $scope.page = 'Splash';
+}]);
+
+lucentApp.controller('homeController', ['$scope', '$location', function($scope, $location){
+  $scope.page = 'Home';
+}]);
+
+lucentApp.controller('aboutController', ['$scope', function($scope){
+  $scope.page = 'About';
+}]);
+
+lucentApp.controller('contactController', ['$scope', function($scope){
+  $scope.page = 'Contact';
+}]);
+
+lucentApp.controller('productsController', ['$scope', function($scope){
+  $scope.page = 'Products';
+}]);
