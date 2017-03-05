ec9519a59c46853c66bdcb59945a1cd94410caaf
Jordan Melendez <jordan.melendez@finalsite.com>
2017-03-05 01:20:59 -0500

    portfolio and contact form started

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,8 @@ jQuery(function($){
 
   'use strict';
 
+  $('.main-navigation a[href*="' + location.pathname + '"]').addClass('active');
+
   var openMenu = $('.open-menu');
   var closeMenu = $('.close-menu');
   var header = $('header');
@@ -10,8 +12,8 @@ jQuery(function($){
   var win = $(window);
   var width = win.width();
 
+
   function handleLayout() {
-    console.log('called');
     if ($(window).width() < 900) {
       header.addClass('closed');
       main.addClass('menu-closed');
@@ -20,13 +22,30 @@ jQuery(function($){
       main.removeClass('menu-closed');
     }
   }
+
+  function contactForm() {
+    $('.contact-link').on('click', function(e){
+      e.preventDefault();
+      $('.contact-form').toggleClass('contact-open');
+    });
+
+    $('.close-contact').on('click', function(){
+      $('.contact-form').removeClass('contact-open');
+    });
+  }
+
+
+
   handleLayout();
+
   $(document).on('load', function() {
 
   });
 
   $(document).ready(function() {
     console.log('document is ready');
+
+    contactForm();
 
     closeMenu.on('click', function() {
       header.toggleClass('closed');
@@ -48,7 +67,6 @@ jQuery(function($){
     var win = $(this);
     var width = win.width();
     handleLayout();
-    console.log(width);
   });
 
 }); //jQuery
