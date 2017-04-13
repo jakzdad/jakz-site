48b41147af4ec9972243ef6cbdc7fcfc7a2c701f
Jordan Melendez <jmelendez92@me.com>
2017-04-13 08:14:52 -0400

    about me timeline started

--- build/js/main.js
+++ build/js/main.js
@@ -3,6 +3,18 @@ jQuery(function($){
   'use strict';
 
   $('.main-navigation a[href*="' + location.pathname + '"]').addClass('active');
-  
+
+  function timelineSlider() {
+    $('.timeline').find('.timeline-list').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      vertical: true,
+      focusOnSelect: true,
+      centerMode: true,
+      initialSlide: 1
+    });
+  }
+
+  timelineSlider();
 
 }); //jQuery
